c4efe915b633f3d879cb41f12d0e097c05d1231e
yebeenhacked <70336790+yebeenhacked@users.noreply.github.com>
2021-10-07 00:10:20 -0400

    Main looking code

---
+++ Main game testing area/Assets/MouseLook.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    float turnSpeed = 120f;
+    float headUpperAngleLimit = 85f;
+    float headLowerAngleLimit = -80f;
+
+    float yaw = 0f;
+    float pitch = 0f;
+
+    Quaternion bodyStartOrientation;
+    Quaternion headStartOrientatiob;
+
+    Transform head;
+
+    private void Start()
+    {
+        head = GetComponentInChildren<Camera>().transform;
+        bodyStartOrientation = transform.localRotation;
+        headStartOrientatiob = head.transform.localRotation;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void FixedUpdate()
+    {
+        var horizontal = Input.GetAxis("Mouse X") * Time.deltaTime * turnSpeed;
+        var vertical = Input.GetAxis("Mouse Y") * Time.deltaTime * turnSpeed;
+
+        yaw += horizontal;
+        pitch += vertical;
+
+        pitch = Mathf.Clamp(pitch, headLowerAngleLimit, headUpperAngleLimit);
+
+        var bodyRotation = Quaternion.AngleAxis(yaw, Vector3.up);
+        var headRotation = Quaternion.AngleAxis(pitch, Vector3.left);
+
+        transform.localRotation = bodyRotation * bodyStartOrientation;
+        head.localRotation = headRotation * headStartOrientatiob;
+    }
+
+
+
+
+
+}
