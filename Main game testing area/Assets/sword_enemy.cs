7150621a7c28a48c27055d21d9516b00043f4528
yebeenhacked <70336790+yebeenhacked@users.noreply.github.com>
2021-10-07 22:59:19 -0400

    mreo fixes

--- Main game testing area/Assets/sword_enemy.cs
+++ Main game testing area/Assets/sword_enemy.cs
@@ -6,6 +6,47 @@ public class sword_enemy : MonoBehaviour
 {
     public GameObject myPrefab;
 
+    private float mSize = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+
+
+    }
+
+    /*private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            InvokeRepeating("Scale", 0f, .003f);
+
+
+        }
+        if (mSize >= 99)
+        {
+            CancelInvoke("Scale");
+            mSize = 0;
+            GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(0, mSize);
+        }
+    }
+
+    void Scale()
+    {
+
+        if (mSize >= 100)
+        {
+            CancelInvoke("Scale");
+        }
+
+
+        GetComponent<SkinnedMeshRenderer>().SetBlendShapeWeight(0, mSize++);
+    }
+
+    */
+
     private void OnDestroy()
     {
         Vector3 a = transform.position;
