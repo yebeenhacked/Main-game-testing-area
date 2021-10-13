b84570ce54423629514a89eb990fdc47aefac819
yebeenhacked <70336790+yebeenhacked@users.noreply.github.com>
2021-10-13 07:35:18 -0400

    grass

--- Main game testing area/Assets/Scripts/spawner.cs
+++ Main game testing area/Assets/Scripts/spawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class spawner : MonoBehaviour
 {
-    gen locate;
+    
     public GameObject en1;
     public GameObject en2;
     public GameObject en3;
@@ -18,7 +18,7 @@ public class spawner : MonoBehaviour
     void Update()
     {
         Debug.Log(currentWave);
-        locate = GetComponent<gen>();
+        
 
         if (transform.childCount <= 0 && currentWave < waves )
         {
@@ -26,21 +26,21 @@ public class spawner : MonoBehaviour
             
             for (int i = 1; i <= dif; i++)
             {
-                spawn(en1, locate);
-                spawn(en2, locate);
-                spawn(en3, locate);
-                spawn(en4, locate);
-                spawn(en5, locate);
-                spawn(en6, locate);
+                spawn(en1);
+                spawn(en2);
+                spawn(en3);
+                spawn(en4);
+                spawn(en5);
+                spawn(en6);
             }
             dif++;
             currentWave++;
         }
     }
 
-    void spawn(GameObject myPrefab, gen a)
+    void spawn(GameObject myPrefab)
     {
-        var randomPosition = new Vector3(Random.Range((a.xsize/2) - 20, (a.xsize/2)+20), Random.Range(10, 10), Random.Range((a.zsize / 2) - 20, (a.zsize / 2) + 20));
+        var randomPosition = new Vector3(Random.Range((250/2) - 20, (250/2)+20), Random.Range(10, 10), Random.Range((250 / 2) - 20, (250 / 2) + 20));
         var enemy = Instantiate(myPrefab, randomPosition, Quaternion.identity);
         enemy.transform.parent = gameObject.transform;
     }
